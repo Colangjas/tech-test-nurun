f58495b3be24eb0450cd2ca1c230aa2a011dc818
Colin Thompson <thompsoncolinm@gmail.com>
2021-05-11 12:39:53 -0400

    closest to zero solved

---
+++ script.js
@@ -0,0 +1,24 @@
+let ts = [-5, -4, -2, 12, -40, 4,  2, 18, 11, 5];
+
+function computeClosestToZero(ts) {
+    // Write your code here
+    // To debug: console.error('Debug messages...');
+
+    console.error(ts);
+    if(ts.length > 1){
+        let result = ts[0];
+        for (let i = 0; i<ts.length; i++){
+            if(Math.abs(ts[i]) < Math.abs(result)){
+
+                result = ts[i];
+            }
+        }
+        console.error(result);
+        
+        return result;
+    } else {
+        return 0;
+    }
+}
+
+computeClosestToZero(ts);
